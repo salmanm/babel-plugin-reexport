d080b5726ce70a2a25bbbcea5596841b3565092e
Salman <salmanmitha@gmail.com>
2019-10-02 14:13:38 -0600

    added standard linting

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-import Path from 'path';
+import Path from 'path'
 
-export default function({ types: t }) {
+export default function ({ types: t }) {
   return {
     visitor: {
       Program: {
-        exit(path, state) {
+        exit (path, state) {
           fn(path, state, t)
         }
       }
-    },
-  };
+    }
+  }
 }
 
 const defaultOpts = {
@@ -17,58 +17,58 @@ const defaultOpts = {
 }
 
 function fn (path, state, t) {
-  const program = path.scope.getProgramParent().path;
+  const program = path.scope.getProgramParent().path
 
-  const nodes = program.get('body');
-  const hasExportDefault = nodes.some(n => n.isExportDefaultDeclaration());
-  const hasExportNamed = nodes.some(n => n.isExportNamedDeclaration());
-  const hasExportAll = nodes.some(n => n.isExportAllDeclaration());
+  const nodes = program.get('body')
+  const hasExportDefault = nodes.some(n => n.isExportDefaultDeclaration())
+  const hasExportNamed = nodes.some(n => n.isExportNamedDeclaration())
+  const hasExportAll = nodes.some(n => n.isExportAllDeclaration())
   const namedDefaultNode = hasExportNamed && nodes.find(n => {
-    if (!n.isExportNamedDeclaration() || !n.has('specifiers')) return false;
+    if (!n.isExportNamedDeclaration() || !n.has('specifiers')) return false
 
-    const specifiers = n.get('specifiers');
-    const { exported } = specifiers[0].node;
-    return exported.name === 'default';
-  });
+    const specifiers = n.get('specifiers')
+    const { exported } = specifiers[0].node
+    return exported.name === 'default'
+  })
 
-  const { moduleDir } = { ...defaultOpts, ...state.opts };
-  const filePath = state.filename;
-  const pkgDir = Path.dirname(filePath.substr(filePath.indexOf(moduleDir) + moduleDir.length));
-  const fileName = Path.basename(filePath, Path.extname(filePath));
-  const exportPath = Path.join(pkgDir, fileName);
+  const { moduleDir } = { ...defaultOpts, ...state.opts }
+  const filePath = state.filename
+  const pkgDir = Path.dirname(filePath.substr(filePath.indexOf(moduleDir) + moduleDir.length))
+  const fileName = Path.basename(filePath, Path.extname(filePath))
+  const exportPath = Path.join(pkgDir, fileName)
 
-  const exportNodes = [];
+  const exportNodes = []
 
   if (hasExportDefault) {
     exportNodes.push(
       getExportNamed(exportPath, ['default'], t)
-    );
+    )
   }
 
   if (namedDefaultNode) {
     exportNodes.push(
       getExportNamed(exportPath, namedDefaultNode.get('specifiers').map(s => s.node.exported.name), t)
-    );
+    )
   }
 
   if (hasExportAll || (hasExportNamed && !namedDefaultNode)) {
     exportNodes.push(
       getExportAll(exportPath, t)
-    );
+    )
   }
 
-  nodes.forEach(n => n.remove());
-  exportNodes.forEach(n => program.pushContainer('body', n));
+  nodes.forEach(n => n.remove())
+  exportNodes.forEach(n => program.pushContainer('body', n))
 }
 
 /** Generates line like `export { a, b } from 'pkg-dir/file'` */
-function getExportNamed(exportPath, identifiers, t) {
+function getExportNamed (exportPath, identifiers, t) {
   const specifiers = identifiers.map(name => t.exportSpecifier(t.identifier(name), t.identifier(name)))
 
-  return t.exportNamedDeclaration(null, specifiers, t.stringLiteral(exportPath));
+  return t.exportNamedDeclaration(null, specifiers, t.stringLiteral(exportPath))
 }
 
 /** Generates line like `export * from 'pkg-dir/file'` */
-function getExportAll(exportPath, t) {
-  return t.exportAllDeclaration(t.stringLiteral(exportPath));
+function getExportAll (exportPath, t) {
+  return t.exportAllDeclaration(t.stringLiteral(exportPath))
 }
--- test/index.js
+++ test/index.js
@@ -1,37 +1,39 @@
-import path from 'path';
-import fs from 'fs';
-import assert from 'assert';
-import { transformFileSync } from '@babel/core';
+/* eslint-env mocha */
 
-import plugin from '../src';
+import path from 'path'
+import fs from 'fs'
+import assert from 'assert'
+import { transformFileSync } from '@babel/core'
 
-const moduleDir = '/actual/';
+import plugin from '../src'
+
+const moduleDir = '/actual/'
 const babelOptions = {
   presets: [
-    ["@babel/env", { "modules": false }],
-    ["@babel/react"],
+    ['@babel/env', { modules: false }],
+    ['@babel/react']
   ],
   plugins: [
-    [plugin, { moduleDir }],
+    [plugin, { moduleDir }]
   ],
   babelrc: false
-};
+}
 
 describe('Re-exports given module', () => {
-  const pkgDir = path.join(__dirname, 'fixtures', moduleDir, 'dummy-pkg');
-  const expDir = path.join(__dirname, 'fixtures', 'expected', 'dummy-pkg');
+  const pkgDir = path.join(__dirname, 'fixtures', moduleDir, 'dummy-pkg')
+  const expDir = path.join(__dirname, 'fixtures', 'expected', 'dummy-pkg')
 
   fs.readdirSync(pkgDir)
     .filter(d => !d.startsWith('.'))
     .map((dirName) => {
       it(`should re-export ${dirName}`, () => {
-        const actualPath = path.join(pkgDir, dirName, 'actual.js');
-        const expectedPath = path.join(expDir, dirName, 'expected.js');
+        const actualPath = path.join(pkgDir, dirName, 'actual.js')
+        const expectedPath = path.join(expDir, dirName, 'expected.js')
 
-        const actual = transformFileSync(actualPath, babelOptions).code;
-        const expected = fs.readFileSync(expectedPath, 'utf8');
+        const actual = transformFileSync(actualPath, babelOptions).code
+        const expected = fs.readFileSync(expectedPath, 'utf8')
 
-        assert.equal(actual.trim(), expected.trim());
-      });
-    });
-});
+        assert.strictEqual(actual.trim(), expected.trim())
+      })
+    })
+})
