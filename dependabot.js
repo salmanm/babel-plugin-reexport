7ad3f7fe5b7c5fa1c2cfcd8845147e67787ca768
Salman <salmanmitha@gmail.com>
2020-11-11 16:33:15 +0530

    chore: separate file for automerge

---
+++ dependabot.js
@@ -0,0 +1,10 @@
+module.exports = async ({
+  github,
+  context
+}) => {
+  // const owner = context.payload.repository.owner.login
+  // const repo = context.payload.repository.name
+  // const prNumber = context.payload.pull_request.number
+
+  console.log('---------+++++++++++++=============')
+}
